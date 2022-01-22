de1982945369300c287e75cea3f6ad89b75187c0
Natasha Laurie <natusik711@gmail.com>
2022-01-22 19:20:50 +0300

    feat: add layout task

--- app/src/main/java/com/example/tms_homework19/MainActivity.kt
+++ app/src/main/java/com/example/tms_homework19/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tms_homework19
 
-import android.support.v7.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnScoreActivity = findViewById<Button>(R.id.btn1)
+
+        btnScoreActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity, ScoreActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
